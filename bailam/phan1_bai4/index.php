0f49e6749e0b904dd3d0f9045f8b8115e1ec2628
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-09 23:00:51 +0700

    toan

--- bailam/phan1_bai4/index.php
+++ bailam/phan1_bai4/index.php
@@ -9,7 +9,7 @@
 <body>
         <?php
         // 
-        $command = '<table class="border-collapse border border-slate-600">';
+        $command = '<table class="border-collapse border border-slate-600 mx-auto">';
         // declared val for increment
         $helo = 1;
         for ($i = 1; $i <= 7; $i += 1){
