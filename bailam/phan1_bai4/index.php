b7744ab46fdb2cabe03b6fa09e73c0312a6d639b
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-09 22:57:00 +0700

    up

--- bailam/phan1_bai4/index.php
+++ bailam/phan1_bai4/index.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
         <?php
         // 
-        $command = "<table>";
+        $command = '<table>';
         // declared val for increment
         $helo = 1;
         for ($i = 1; $i <= 7; $i += 1){
             $max = $helo * 7;
-            $command .= "<tr>";
+            $command .= '<tr class="text-center">';
             for ($j = $i; $j <= $max; $j += $helo){
-                $command .= "<td>".$j."</td>";
+                $command .= '<td class="text-amber-400 p-1">'.$j."</td>";
             }
             $helo += 1;
             $command .= "</tr>";
