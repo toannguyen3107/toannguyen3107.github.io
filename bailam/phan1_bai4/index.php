fbe3492cb777bdbd6927984f5ce85f16c4664d4f
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-09 22:39:29 +0700

    update

--- bailam/phan1_bai4/index.php
+++ bailam/phan1_bai4/index.php
@@ -10,12 +10,13 @@
         <?php
         // 
         $command = "<tr>"
+        // declared val for increment
         $helo = 1;
         for (let $i = 1; $i <= 7; $i += 1){
             $helo += 1
             $max = $helo * 7
-            for (let $j = $i; $j <$max; $j += $helo){
-                $command .= "<tr>".${$j + $helo}."</tr>";
+            for (let $j = $i; $j < $max; $j += $helo){
+                $command .= "<tr>".{$j + $helo}."</tr>";
             }
         }
         $command .= "</tr>";
