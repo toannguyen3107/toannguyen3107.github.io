7976e3ddf3a4d6c51fafca446457d5ce0976ade5
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-09 21:57:47 +0700

    update p1b2l3

--- bailam/phan1_bai2/index.php
+++ bailam/phan1_bai2/index.php
@@ -7,27 +7,28 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <h1>hello world</h1>
     <?php
     $param1 = 100;
     $test = $param1 % 5;
-    function handler($params){
-        case 0:
-            echo "Hello";
-            break;
-        case 1:
-            echo "How are you?"
-            break;
-        case 2:
-            echo "I'm doing well, thank you";
-            break;
-        case 3:
-            echo "See you later";
-            break;
-        case 4:
-            echo "Good-bye"
-            break;
-    }
-    handler($test);
+        function handler($params){
+            case 0:
+                echo "Hello";
+                break;
+            case 1:
+                echo "How are you?"
+                break;
+            case 2:
+                echo "I'm doing well, thank you";
+                break;
+            case 3:
+                echo "See you later";
+                break;
+            case 4:
+                echo "Good-bye"
+                break;
+        }
+        handler($test);
     ?>
 
 </body>
