d4bce3890b0bb92fd8113f1eeb256cd25448e9c3
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-10 09:28:39 +0700

    toan

--- bailam/phan1_bai5/script.js
+++ bailam/phan1_bai5/script.js
@@ -1,9 +1,6 @@
-
-function handler(event){
-    if (document.getElementById('res').value === '0'){
-        document.getElementById('res').value = event.currentTarget.value;
-    }
-    else{
-        document.getElementById('res').value += event.currentTarget.value;
-    }            
+function handler(e) {
+    const resultInput = document.getElementById('res');
+    const currentValue = resultInput.value;
+    console.log(e.currentTarget.value);
+    resultInput.value = currentValue === '0' ? e.currentTarget.value : currentValue + e.currentTarget.value;
 }
