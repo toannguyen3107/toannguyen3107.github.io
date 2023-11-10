ebcaf601322829d471d45178fc97b17e540d082a
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-10 09:17:03 +0700

    toan

--- bailam/phan1_bai5/index.php
+++ bailam/phan1_bai5/index.php
@@ -13,8 +13,8 @@
 
         }
     ?>
-    <form action="./index.php" method="get" class="w-8/12 flex flex-row flex-wrap bg-green-400 p-3 align-center border-2 border-4 border-indigo-500/100 rounded-lg mx-auto justify-center" id="res" value="<?php echo $result;?>">
-    <input type="text" class="bg-slate-100 w-full p-2 border-2 border-black rounded-xl mb-3" disabled id="result">
+    <form action="./index.php" method="get" class="w-8/12 flex flex-row flex-wrap bg-green-400 p-3 align-center border-2 border-4 border-indigo-500/100 rounded-lg mx-auto justify-center" >
+    <input type="text" class="bg-slate-100 w-full p-2 border-2 border-black rounded-xl mb-3" disabled id="res" value="<?php echo $result;?>">
     <div class="w-3/12">
         <button type="button" class="w-10/12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" value="+">+</button>
     </div>
