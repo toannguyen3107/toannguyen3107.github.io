f56e1a5482d0c7dac49677e6464ca008d7a5fb5d
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-10 08:47:28 +0700

    toan

--- bailam/phan1_bai5/index.php
+++ bailam/phan1_bai5/index.php
@@ -36,7 +36,7 @@
     </div>
     
     <div class="w-3/12">
-        <button type="button" class="w-10/12 w-3/12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">^</button>
+        <button type="button" class="w-10/12 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">^</button>
     </div>
     
     <div class="w-3/12">
@@ -62,7 +62,7 @@
         <button type="button" class="w-10/12 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">9</button>
     </div>
     <div class="w-3/12">
-            <button type="submit"  class="w-10/12 text-gray-900 bg-slate-100 border border-gray-500 rounded-xl">=</button>
+            <button type="submit"  class="w-10/12 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">=</button>
     </div>
 
     </form>
