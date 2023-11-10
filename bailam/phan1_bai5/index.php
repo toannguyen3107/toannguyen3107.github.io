dc7f9c07dffb43c9e3eaa58e91a136223f7e57e2
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-10 07:56:57 +0700

    toan

--- bailam/phan1_bai5/index.php
+++ bailam/phan1_bai5/index.php
@@ -11,7 +11,7 @@
         if(isset($_GET['']))
     ?>
     <form action="./index.php" method="get" class="flex flex-row flex-wrap gap-1 bg-red-400">
-    <input type="text bg-slate-100 w-11/12" disabled id="result">
+    <input type="text" class="bg-slate-100 w-11/12" disabled id="result">
     <button type="button" class="w-1/4 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">1</button>
     <button type="button" class="w-1/4 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">2</button>
     <button type="button" class="w-1/4 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">3</button>
