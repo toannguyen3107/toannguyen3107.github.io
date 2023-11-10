ac01eb4ebdb8a7518a6e1ed4577b346f7ba3ed0b
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-10 07:26:56 +0700

    up

---
+++ bailam/phan1_bai5/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phan 1 - bai 5</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+    <form action="/index.php" method="get" class="flex flex-row flex-wrap">
+        <button class="btn btn-white">1</button>
+    </form>
+</body>
+</html>
