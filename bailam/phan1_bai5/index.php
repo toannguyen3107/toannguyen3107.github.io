073629c9193bc267b72b4008178e88e3a93a2291
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-10 08:06:49 +0700

    toan

--- bailam/phan1_bai5/index.php
+++ bailam/phan1_bai5/index.php
@@ -10,7 +10,7 @@
     <?php
         if(isset($_GET['']))
     ?>
-    <form action="./index.php" method="get" class=" flex flex-row flex-wrap bg-red-400 p-3 align-center justify-center">
+    <form action="./index.php" method="get" class=" flex flex-row flex-nowrap bg-red-400 p-3 align-center justify-center">
     <input type="text" class="bg-slate-100 w-full rouded-xl" disabled id="result">
     <button type="button" class="w-4/12 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">1</button>
     <button type="button" class="w-4/12 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">2</button>
