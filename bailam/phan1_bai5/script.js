340aff188c86f16feaa461be0d5b41011ce4af6f
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-10 09:30:48 +0700

    p

--- bailam/phan1_bai5/script.js
+++ bailam/phan1_bai5/script.js
@@ -2,5 +2,5 @@ function handler(e) {
     const resultInput = document.getElementById('res');
     const currentValue = resultInput.value;
     console.log(e.currentTarget.value);
-    resultInput.value = currentValue === '0' ? e.currentTarget.value : currentValue + e.currentTarget.value;
+    resultInput.value = currentValue === '0' ? e.target.value : currentValue + e.target.value;
 }
