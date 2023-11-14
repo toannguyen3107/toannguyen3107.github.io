1d79b75ecd82f1acd7fba3711717e43553be563a
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-14 16:11:22 +0700

    toan

--- bailam/phan1_bai6/func.php
+++ bailam/phan1_bai6/func.php
@@ -9,7 +9,7 @@
                 if ($len < 2 || $len > 30) {
                     $mess = "<div class=\"text-danger class form-text\" id=\"fnameHelp\">The length of the name must be greater than 2 and less than 30!</div>";
                 }else{
-                    $regex = "/^[a-zA-Z]+[' -]{0,1}[a-zA-Z]+/";
+                    $regex = "/^[a-zA-Z]+[' -]?[a-zA-Z]*$/";
                     if (!preg_match($regex, $fname)) {
                         $mess = "<div class=\"text-danger class form-text\" id=\"fnameHelp\">Error</div>";
                     }
@@ -33,7 +33,7 @@ function checkLastName() {
             if ($len < 2 || $len > 30) {
                 $mess = "<div class=\"text-danger class form-text\" id=\"lnameHelp\">The length of the name must be greater than 2 and less than 30!</div>";
             }else{
-                $regex = "/^[a-zA-Z]+[' -]{0,1}[a-zA-Z]+/";
+                $regex = "/^[a-zA-Z]+[' -]?[a-zA-Z]*$/";
                 if (!preg_match($regex, $lname)) {
                     $mess = "<div class=\"text-danger class form-text\" id=\"lnameHelp\">Error</div>";
                 }
