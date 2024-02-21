491b84992163c31edba9c7c8b31f190e57053a31
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2024-02-22 00:20:59 +0700

    dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className='dark'>
       <body>
         <Providers>
-          <Nav />
-          {children}
+          <div className="flex flex-row gap-2">
+            <Nav className="sticky top-0 w-2/12 bg-[rgb(86,93,95)] bg-[linear-gradient(90deg, rgba(86,93,95,1) 0%, rgba(76,76,83,1) 31%, rgba(171,196,203,1) 68%, rgba(193,224,231,1) 99%)] h-[100vh]" />
+            <div className="w-10/12">{children}</div>
+          </div>
+
         </Providers>
       </body>
     </html>
