df8006d632c3219c76f83e9401911adf4203663e
toancse <minhtoanaglx@gmail.com>
2024-02-29 18:33:00 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function App() {
   return (
-    <p className="absolute top-1/2  left-1/2 translate-x-[-50%] translate-y-[-50%]">Updating</p>
+    <p className="absolute top-1/2  left-1/2 translate-x-[-50%] translate-y-[-50%]">Updating!</p>
   );
 }
