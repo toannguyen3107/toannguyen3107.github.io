492a461a4aefb79a0f3b38e87eac111c61b9d78c
toancse <minhtoanaglx@gmail.com>
2024-03-16 02:38:23 +0700

    update
    i change name article into `posts` and update redirect with useRoute

---
+++ app/tags/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return (
+        <div>tag page</div>
+    )
+}
---
+++ config/post.tsx
@@ -0,0 +1,10 @@
+export const list_post = [
+    {
+        name: 'picoctf2024',
+        tag: ['ctf', 'picoctf', 'cybersecurity', 'webexploit', 'crypto'],
+        date: new Date('2023-03-16'),
+        path: 'picoctf2024'
+    }
+];
+export const author = 'Nguyen Minh Toan';
+export const tag = ['ctf', 'picoctf', 'cybersecurity', 'webexploit', 'crypto'];
