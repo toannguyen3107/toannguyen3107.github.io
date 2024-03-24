f78ae9b0fdcfa0b9ce537328ab648538047f4a37
toancse <minhtoanaglx@gmail.com>
2024-03-24 18:15:41 +0700

    update

--- config/post.tsx
+++ config/post.tsx
@@ -2,7 +2,7 @@ export const list_post = [
     {
         header: 'Wifinetic',
         subtitle: 'Hackthebox',
-        tag: ['ctf', 'picoctf', 'cybersecurity', 'webexploit'],
+        tag: ['ctf', 'picoctf', 'cybersecurity', 'webexploit', 'hackthebox'],
         date: new Date('2024-04-20').toISOString().slice(0, 10),
         path: '/posts/wifinetictwo',
         img: '/images/wifinetictwo.png'
@@ -10,9 +10,16 @@ export const list_post = [
         header: 'Passcode',
         subtitle: 'pwnable.kr',
         tag: ['ctf', 'pwn', 'cybersecurity', 'binaryexploit'],
-        date: new Date('2024-04-24').toISOString().slice(0, 10),
+        date: new Date('2024-04-23').toISOString().slice(0, 10),
         path: '/posts/passcode',
         img: '/images/passcode/passcode.png'
+    },{
+        header: 'Racecar',
+        subtitle: 'Hackthebox',
+        tag: ['ctf', 'pwn', 'cybersecurity', 'binaryexploit','hackthebox'],
+        date: new Date('2024-04-24').toISOString().slice(0, 10),
+        path: '/posts/racecar',
+        img: '/images/racecar/racecar.png'
     }
 ];
 export const author = 'Nguyen Minh Toan';
