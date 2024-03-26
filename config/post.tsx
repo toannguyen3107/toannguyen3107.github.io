65532ba7166850a4bc7618cd355a1a4217f09f1d
toancse <minhtoanaglx@gmail.com>
2024-03-26 10:30:34 +0700

    update

--- config/post.tsx
+++ config/post.tsx
@@ -1,25 +1,34 @@
 export const list_post = [
     {
-        header: 'Wifinetic',
+        header: 'Headless',
         subtitle: 'Hackthebox',
-        tag: ['ctf', 'picoctf', 'cybersecurity', 'webexploit', 'hackthebox'],
-        date: new Date('2024-04-20').toISOString().slice(0, 10),
-        path: '/posts/wifinetictwo',
-        img: '/images/wifinetictwo.png'
-    },{
+        tag: ['ctf', 'webexploit', 'cybersecurity', 'binaryexploit', 'hackthebox'],
+        date: new Date('2024-04-26').toISOString().slice(0, 10),
+        path: '/posts/headless',
+        img: '/images/headless/headless.png'
+    },
+    {
+        header: 'Racecar',
+        subtitle: 'Hackthebox',
+        tag: ['ctf', 'pwn', 'cybersecurity', 'binaryexploit', 'hackthebox'],
+        date: new Date('2024-04-24').toISOString().slice(0, 10),
+        path: '/posts/racecar',
+        img: '/images/racecar/racecar.png'
+    }, {
         header: 'Passcode',
         subtitle: 'pwnable.kr',
         tag: ['ctf', 'pwn', 'cybersecurity', 'binaryexploit'],
         date: new Date('2024-04-23').toISOString().slice(0, 10),
         path: '/posts/passcode',
         img: '/images/passcode/passcode.png'
-    },{
-        header: 'Racecar',
+    },
+    {
+        header: 'Wifinetic',
         subtitle: 'Hackthebox',
-        tag: ['ctf', 'pwn', 'cybersecurity', 'binaryexploit','hackthebox'],
-        date: new Date('2024-04-24').toISOString().slice(0, 10),
-        path: '/posts/racecar',
-        img: '/images/racecar/racecar.png'
+        tag: ['ctf', 'picoctf', 'cybersecurity', 'webexploit', 'hackthebox'],
+        date: new Date('2024-04-20').toISOString().slice(0, 10),
+        path: '/posts/wifinetictwo',
+        img: '/images/wifinetictwo.png'
     }
 ];
 export const author = 'Nguyen Minh Toan';
